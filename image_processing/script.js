1039559f3c12082e02db684c85ac4edf8b4893ca
LilyLaw <1444987087@qq.com>
2019-03-05 09:58:18 +0800

    使用离屏canvas为图片加上水印

--- image_processing/script.js
+++ image_processing/script.js
@@ -1,28 +1,37 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var img = new Image();
 var slider = document.getElementById("scale-range");
-
+var watermarkCanvas = document.getElementById("watermark-canvas");
+var watermarkContext = watermarkCanvas.getContext("2d");
 
 window.onload = function(){
 	canvas.width = 1152;
 	canvas.height = 768;
 
+	var img = new Image();
 	img.src = "./timg.jpeg";
 	img.onload = function(){
 		var scale = slider.value;
-		drawImageByScale( scale );
+		drawImageByScale(img, scale );
 
 		slider.onmousemove = function(){
 			var scaleCh = slider.value;
-			drawImageByScale(scaleCh);
+			drawImageByScale(img,scaleCh);
 		}
 
 		// context.drawImage(img,0,0,canvas.width,canvas.height);	// 充满整个画布
+
 	}
+	// set watermark canvas
+	watermarkCanvas.width = 360;
+	watermarkCanvas.height = 100;
+	watermarkContext.font = "bold 50px Arial";
+	watermarkContext.fillStyle = "rgba(255,255,255,0.5)";
+	watermarkContext.textBaleline = "middle";
+	watermarkContext.fillText("== LilyLaw ==",20,50);
 }
 
-function drawImageByScale( scale ){
+function drawImageByScale( img,scale ){
 	var imgWidth = 1152*scale;
 	var imgHeight = 768*scale;
 	var dx = canvas.width/2 - imgWidth/2;
@@ -33,4 +42,6 @@ function drawImageByScale( scale ){
 
 	// 再重新绘制画布
 	context.drawImage(img,dx,dy,imgWidth,imgHeight);
+	context.drawImage(watermarkCanvas,canvas.width - watermarkCanvas.width,
+									  canvas.height - watermarkCanvas.height);
 }
